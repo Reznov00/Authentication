d32357b79154f77bd088afe13b66f29c923d40e9
reznov00 <cena.amz@gmail.com>
2021-03-11 04:49:17 +0500

    Store passwords in hash

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 app.use(express.static("public"));
@@ -17,8 +17,6 @@ const userSchema = new mongoose.Schema({
   email : String,
   password : String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model ("User",userSchema);
 
@@ -42,7 +40,7 @@ app.get("/logout",function(req,res){
 app.post("/register",function(req,res){
   const newUser = new User({
     email : req.body.username,
-    password : req.body.password
+    password : md5(req.body.password)
   });
   newUser.save(function(err){
     if(!err){
@@ -58,7 +56,7 @@ app.post("/login",function(req,res){
   User.findOne({email : req.body.username}, function(err,foundUser){
     if(!err){
       if(foundUser){
-        if(foundUser.password === req.body.password){
+        if(foundUser.password === md5(req.body.password)){
           res.render("secrets");
         }
       }
